f0c055c1eb771c4bf8480f884b38382dbb4f78cd
SupriyaS3 <supriyaperiyasamy24@gmail.com>
2021-09-05 13:45:04 +0530

    Login_Edited

---
+++ Login/app/src/main/java/com/masterandroid/login/MainActivity.java
@@ -0,0 +1,24 @@
+package com.masterandroid.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        FacebookSdk.sdkInitialize(getApplicationContext());
+
+    }
+}
